b9c7cd22ba571a502211a88eb13b3800f5e089f8
Bobby O'Brien <contact@bobbyobrien.com>
2025-05-03 17:56:22 -0500

    Skeleton project outline

---
+++ LLClient/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Protocol.h"    // from LLSharedLib
+
+int main()
+{
+    std::cout << "LicenseLock LLClient starting up..." << std::endl;
+
+    // TODO: replace with actual ClientApp orchestration
+    // e.g. ClientApp app; return app.run();
+
+    return 0;
+}
